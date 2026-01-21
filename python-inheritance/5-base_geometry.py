f5895544030616f84b64556b33e3ed0e1eff13f6
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 08:39:48 +0000

    Add task 5: Empty BaseGeometry class

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the BaseGeometry class.
+This class will serve as a base for future geometry-related classes.
+"""
+
+
+class BaseGeometry:
+    """
+    An empty class BaseGeometry.
+    """
+    pass

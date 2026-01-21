9c1e1254330b7eab1d39cc548eb953ab1050131c
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 09:03:48 +0000

    Add task 1: write_file function

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-write_file
+Contains function that writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8)
+    Args:
+        filename (str): name of the file
+        text (str): text to write
+    Returns:
+        The number of characters written
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)

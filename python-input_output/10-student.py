2fd4448a52cc4dc4c66e496b50d5698af9deb172
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 09:18:32 +0000

    Fix PEP8 E501: simplified logic to reduce line length

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -11,10 +11,6 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Initialization of the student
-        Args:
-            first_name (str): first name
-            last_name (str): last name
-            age (int): age
         """
         self.first_name = first_name
         self.last_name = last_name
@@ -23,13 +19,12 @@ class Student:
     def to_json(self, attrs=None):
         """
         Retrieves a dictionary representation of a Student instance
-        If attrs is a list of strings, only attributes in the list are retrieved
         """
-        if (isinstance(attrs, list) and
-                all(isinstance(s, str) for s in attrs)):
-            res = {}
-            for k, v in self.__dict__.items():
-                if k in attrs:
-                    res[k] = v
-            return res
+        if isinstance(attrs, list):
+            if all(isinstance(s, str) for s in attrs):
+                res = {}
+                for k, v in self.__dict__.items():
+                    if k in attrs:
+                        res[k] = v
+                return res
         return self.__dict__

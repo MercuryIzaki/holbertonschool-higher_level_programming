7873e9e8c4da463c3bbfdcbcf2da7cf40af574df
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 11:57:11 +0000

    Add tasks 0 to 6 of SQL more queries

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,4 +1,4 @@
--- Creates the table force_name with id (INT) and name (VARCHAR(256) NOT NULL)
+-- Creates the table force_name
 CREATE TABLE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,4 +1,4 @@
--- Creates the table unique_id with id INT (default 1, unique) and name VARCHAR(256)
+-- Creates the table unique_id with unique id
 CREATE TABLE IF NOT EXISTS unique_id (
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)

5723a4038a0f80b6880f1e80f4d354164405bc19
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 12:07:12 +0000

    Complete tasks 3, 5, 6, 7 and 8

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,5 +1,4 @@
--- Lists all cities of California from the database hbtn_0d_usa
--- Results sorted by cities.id in ascending order
+-- Lists all cities of California that can be found in the database hbtn_0d_usa
 SELECT id, name FROM cities 
 WHERE state_id = (SELECT id FROM states WHERE name = 'California') 
 ORDER BY id ASC;

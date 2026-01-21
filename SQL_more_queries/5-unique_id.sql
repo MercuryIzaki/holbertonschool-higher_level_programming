f3af6226e8a669aa44f9cfb440c717a4e34de2a1
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 12:00:25 +0000

    Add tasks 5, 6, 7 and 8

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,2 +1,5 @@
--- Creates the table unique_id
-CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+-- Creates the table unique_id with unique id DEFAULT 1
+CREATE TABLE IF NOT EXISTS unique_id (
+    id INT DEFAULT 1 UNIQUE,
+    name VARCHAR(256)
+);
--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,7 +1,7 @@
--- Creates the table cities
+-- Creates the table cities with a foreign key referencing states
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-    id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
     state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)

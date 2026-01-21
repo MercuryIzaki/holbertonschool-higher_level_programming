a6aa646217d6a4c1bb4f3c9db453ed5aab79d3bb
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 09:20:56 +0000

    Fix PEP8: remove whitespace in blank lines

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -18,12 +18,9 @@ def pascal_triangle(n):
     for i in range(1, n):
         row = [1]
         prev_row = triangle[i - 1]
-        
-        # Вычисляем внутренние элементы строки
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
-            
-        row.append(1)  # Замыкающая единица
+        row.append(1)
         triangle.append(row)
 
     return triangle

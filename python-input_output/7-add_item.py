eb0ae7e3cf729fce41904d2f4b6dd28e080b3877
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 09:14:19 +0000

    Fix PEP8: line too long in task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,18 +6,15 @@ import sys
 import os
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = \
+    __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
 
-# Проверяем, существует ли файл, чтобы загрузить данные или создать пустой список
 if os.path.exists(filename):
     items = load_from_json_file(filename)
 else:
     items = []
 
-# Добавляем все аргументы (пропуская имя самого скрипта)
 items.extend(sys.argv[1:])
-
-# Сохраняем обновленный список
 save_to_json_file(items, filename)

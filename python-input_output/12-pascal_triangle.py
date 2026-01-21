401c739ebda33d1e72dc5c5e07fc411df8305a8a
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 09:20:12 +0000

    Add task 12: Pascal's Triangle implementation

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 12-pascal_triangle
+Contains function that returns Pascal's triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing Pascal’s triangle of n
+    Returns an empty list if n <= 0
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+
+    for i in range(1, n):
+        row = [1]
+        prev_row = triangle[i - 1]
+        
+        # Вычисляем внутренние элементы строки
+        for j in range(1, i):
+            row.append(prev_row[j - 1] + prev_row[j])
+            
+        row.append(1)  # Замыкающая единица
+        triangle.append(row)
+
+    return triangle

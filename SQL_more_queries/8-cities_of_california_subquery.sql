2b9b91310912502fe7c813fac7a4ac9106f29d87
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 12:08:14 +0000

    Complete SQL tasks 3, 5, 6, 7, 8 and 9

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,4 @@
--- Lists all cities of California that can be found in the database hbtn_0d_usa
+-- Lists all cities of California from hbtn_0d_usa without using JOIN
 SELECT id, name FROM cities 
 WHERE state_id = (SELECT id FROM states WHERE name = 'California') 
 ORDER BY id ASC;
--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,7 +1,5 @@
--- Lists all cities contained in the database hbtn_0d_usa
--- Each record displays: cities.id - cities.name - states.name
--- Results must be sorted in ascending order by cities.id
-SELECT cities.id, cities.name, states.name
-FROM cities
-INNER JOIN states ON cities.state_id = states.id
+-- Lists all cities and their state names using a JOIN
+SELECT cities.id, cities.name, states.name 
+FROM cities 
+INNER JOIN states ON cities.state_id = states.id 
 ORDER BY cities.id ASC;

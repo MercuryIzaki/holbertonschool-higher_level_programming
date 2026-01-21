e8c226ffe984add920e7d66fa7d9bd18f23e40f2
MercuryIzaki <Izakimercury@gmail.com>
2026-01-21 11:27:44 +0000

    Add task 4: create first_table

--- SQL_introduction/4-first_table.sql
+++ SQL_introduction/4-first_table.sql
@@ -1,4 +1,4 @@
--- Creates a table called first_table with id and name
+-- Creates a table called first_table with id and name in the current database
 CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
